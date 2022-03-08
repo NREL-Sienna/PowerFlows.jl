bfe35338a34bcc932a3e733e53639f1fa8ee1544
Jose Daniel Lara <jdlara@berkeley.edu>
2022-03-08 10:52:45 -0800

    add code for power NLsolve power flow

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,12 @@
 using Test
+using Logging
+using PowerSystems
+import PowerSystems: PowerSystemTableData
+const PSY = PowerSystems
+
+import Aqua
+Aqua.test_unbound_args(PowerFlows)
+Aqua.test_undefined_exports(PowerFlows)
+Aqua.test_ambiguities(PowerFlows)
+Aqua.test_stale_deps(PowerFlows)
+Aqua.test_deps_compat(PowerFlows)
