e992a6b835e8a5eb8915229609f84034cdd5c5f0
Jose Daniel Lara <jdlara@berkeley.edu>
2023-03-28 11:24:18 -0600

    add missing include

--- src/PowerFlows.jl
+++ src/PowerFlows.jl
@@ -16,6 +16,7 @@ const PSY = PowerSystems
 const PNM = PowerNetworkMatrices
 
 include("definitions.jl")
+include("powerflow_types.jl")
 include("PowerFlowData.jl")
 include("dc_powerflow.jl")
 include("nlsolve_ac_powerflow.jl")
