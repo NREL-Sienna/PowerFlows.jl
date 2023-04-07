fadbabd211581bbb9891122439f644b5d5b09c77
Jose Daniel Lara <jdlara@berkeley.edu>
2023-04-06 23:59:06 -0600

    api update for powerflow data

--- src/dc_powerflow.jl
+++ src/dc_powerflow.jl
@@ -8,53 +8,36 @@ Evaluates the power flowing on each system's branch and updates the PowerFlowDat
         PowerFlowData structure containig all the information related to the system power flow
 """
 function solve_powerflow!(
-    ::DCPowerFlow,
-    data::PowerFlowData,
+    ::PTDFDCPowerFlow,
+    sys::PSY.System;
+    parallel = false,
+)
     power_network_matrix::PNM.PTDF,
-    aux_network_matrix::PNM.ABA_Matrix,
-    power_injection::Vector{Float64};
-    parallel = false
-    )
+    aux_network_matrix::PNM.ABA_Matrix, data = PowerFlow
 
-    data.bus_angle[:] .= aux_network_matrix.K \ power_injection
     ref_buses = aux_network_matrix.ref_bus_positions
     # ! setdiff still present since ptdf has also the columns related to the reference buses
     matrix_data = @view power_network_matrix.data[:, setdiff(1:end, ref_buses)]
     if parallel
-        @threads for i in axes(matrix_data, 1)
-            data.branch_flow_values[i] = matrix_data[i, :]' * power_injection
-        end
+        my_mul_mt!(data.branch_flow_values, matrix_data, power_injection)
     else
-        data.branch_flow_values[:] .= matrix_data * power_injection
+        my_mul_single!(data.branch_flow_values, matrix_data, power_injection)
     end
+
+    ldiv!(data.bus_angle, aux_network_matrix.K, power_injection)
+    return
 end
 
 function solve_powerflow!(
     ::DCPowerFlow,
+    sys::PSY.System;
+    parallel = false,
+)
     data::PowerFlowData,
     power_network_matrix::PNM.ABA_Matrix,
     aux_network_matrix::PNM.BA_Matrix,
     power_injection::Vector{Float64}
-    )
-
     data.bus_angle[:] .= power_network_matrix.K \ power_injection
     data.branch_flow_values[:] .= aux_network_matrix.data * data.bus_angle
-end
-
-# get active power injected in a certain bus
-function get_active_power_injection(bus_components::Vector{T}) where T <: PSY.StaticInjection
-    # get the components connected to the considered bus
-    pos_comp = [d for d in filter(x -> !isa(x, PSY.ElectricLoad), bus_components)]
-    neg_comp = [d for d in filter(x -> isa(x, PSY.ElectricLoad), bus_components)]
-    # return the total injection at the cosndiered bus
-    return _sum_active_power(pos_comp) - _sum_active_power(neg_comp)
-end
-
-function _sum_active_power(bus_components::Vector{T}) where T <: PSY.StaticInjection
-    vec_ = [PSY.get_active_power(d) for d in bus_components if typeof(d) != PSY.FixedAdmittance]
-    if length(vec_) > 0
-        return sum(vec_)
-    else
-        return 0
-    end
+    return
 end
