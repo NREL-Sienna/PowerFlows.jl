9217373009c197997a3aad3cf86130cd6b99afe8
alefcastelli <alef.castelli@gmail.com>
2023-04-25 13:03:31 -0600

    fixed test for PTDF and DC PowreFlow

--- test/test_dc_powerflow.jl
+++ test/test_dc_powerflow.jl
@@ -1,9 +1,11 @@
 @testset "DC PowerFlow PTDF" begin
     sys = PSB.build_system(PSB.PSITestSystems, "c_sys14"; add_forecasts = false)
-    solve_powerflow!(PTDFDCPowerFlow(), sys)
+    data = PowerFlowData(PTDFDCPowerFlow(), sys)
+    solve_powerflow!(data)
 end
 
 @testset "DC PowerFlow" begin
     sys = PSB.build_system(PSB.PSITestSystems, "c_sys14"; add_forecasts = false)
-    solve_powerflow!(DCPowerFlow(), sys)
+    data = PowerFlowData(DCPowerFlow(), sys)
+    solve_powerflow!(data)
 end
