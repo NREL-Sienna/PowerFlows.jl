7cb92f778fd954a12eb0aac5c365e2f5ae7240c9
alefcastelli <alef.castelli@gmail.com>
2023-06-29 18:32:52 -0600

    updated PowerFlowData structure and function

--- src/PowerFlowData.jl
+++ src/PowerFlowData.jl
@@ -1,14 +1,15 @@
 struct PowerFlowData{M <: PNM.PowerNetworkMatrix, N}
     bus_lookup::Dict{Int, Int}
     branch_lookup::Dict{String, Int}
-    bus_activepower_injection::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
-    bus_reactivepower_injection::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
-    bus_activepower_withdrawals::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
-    bus_reactivepower_withdrawals::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
+    bus_activepower_injection::Union{Vector{Float64}, Matrix{Float64}}
+    bus_reactivepower_injection::Union{Vector{Float64}, Matrix{Float64}}
+    bus_activepower_withdrawals::Union{Vector{Float64}, Matrix{Float64}}
+    bus_reactivepower_withdrawals::Union{Vector{Float64}, Matrix{Float64}}
     bus_type::Vector{PSY.BusTypes}
-    bus_magnitude::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
-    bus_angles::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
-    branch_flow_values::Union{Vector{Float64}, Matrix{Float64}, SharedArray{Float64}}
+    bus_magnitude::Union{Vector{Float64}, Matrix{Float64}}
+    bus_angles::Union{Vector{Float64}, Matrix{Float64}}
+    branch_flow_values::Union{Vector{Float64}, Matrix{Float64}}
+    timestep_map::Dict{Int, String}
     valid_ix::Vector{Int}
     power_network_matrix::M
     aux_network_matrix::N
@@ -74,10 +75,25 @@ function PowerFlowData(::ACPowerFlow, sys::PSY.System)
 end
 
 # version with full PTDF
-function PowerFlowData(::DCPowerFlow, sys::PSY.System, timesteps::Int, shared::Bool=false)
+function PowerFlowData(
+    ::DCPowerFlow,
+    sys::PSY.System,
+    timesteps::Int,
+    timestep_names::Vector{String} = String[])
+
+    # assign timestep_names
+    # timestep names are then allocated in a dictionary to map matrix columns
+    if timesteps != 0
+        if length(timestep_names) == 0
+            timestep_names = [string(i) for i in 1:timesteps]
+        elseif  length(timestep_names) != timesteps
+            error("timestep_names field must have same length as timesteps")
+        end
+    end
+
+    # get the network matrices
     power_network_matrix = PNM.ABA_Matrix(sys; factorize = true)
     aux_network_matrix = PNM.BA_Matrix(sys)
-    # check the maps betwen the 2 matrices match
 
     # get number of buses and branches
     n_buses = length(axes(aux_network_matrix, 2))
@@ -102,9 +118,10 @@ function PowerFlowData(::DCPowerFlow, sys::PSY.System, timesteps::Int, shared::B
         end
     end
 
+    # define injection vectors related to the first timestep
     bus_activepower_injection = zeros(Float64, n_buses)
     bus_reactivepower_injection = zeros(Float64, n_buses)
-    get_injections!(bus_activepower_injection, bus_reactivepower_injection, bus_lookup, sys)
+    PF.get_injections!(bus_activepower_injection, bus_reactivepower_injection, bus_lookup, sys)
 
     bus_activepower_withdrawals = zeros(Float64, n_buses)
     bus_reactivepower_withdrawals = zeros(Float64, n_buses)
@@ -116,13 +133,10 @@ function PowerFlowData(::DCPowerFlow, sys::PSY.System, timesteps::Int, shared::B
     )
 
     # initialize data
-    if shared
-        init_1 = SharedArray{Float64}(n_buses, timesteps)
-        init_2 = SharedArray{Float64}(n_branches, timesteps)
-    else
-        init_1 = zeros(n_buses, timesteps)
-        init_2 = zeros(n_branches, timesteps)
-    end
+    init_1 = zeros(n_buses, timesteps)
+    init_2 = zeros(n_branches, timesteps)
+
+    # define fields as matrices whose number of columns is eqault to the number of timesteps
     bus_activepower_injection_1 = deepcopy(init_1)
     bus_reactivepower_injection_1 = deepcopy(init_1)
     bus_activepower_withdrawals_1 = deepcopy(init_1)
@@ -131,7 +145,7 @@ function PowerFlowData(::DCPowerFlow, sys::PSY.System, timesteps::Int, shared::B
     bus_angle_1 = deepcopy(init_1)
     branch_flow_values_1 = deepcopy(init_2)
 
-    # substitue initial values for initial timestep
+    # initial values related to first timestep allocated in the first column
     bus_activepower_injection_1[:, 1] .= bus_activepower_injection
     bus_reactivepower_injection_1[:, 1] .= bus_reactivepower_injection
     bus_activepower_withdrawals_1[:, 1] .= bus_activepower_withdrawals
@@ -151,6 +165,7 @@ function PowerFlowData(::DCPowerFlow, sys::PSY.System, timesteps::Int, shared::B
         bus_magnitude_1,
         bus_angle_1,
         branch_flow_values_1,
+        Dict(zip([i for i in 1:timesteps], timestep_names)),
         setdiff(1:n_buses, aux_network_matrix.ref_bus_positions),
         power_network_matrix,
         aux_network_matrix,
