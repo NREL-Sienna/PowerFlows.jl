eb34f8fb8d6b7f01961a198330aff2b8ae78e3ce
Jose Daniel Lara <jdlara@berkeley.edu>
2023-10-30 12:21:19 -0600

    add kwarg constructor for AC power flow

--- src/powerflow_types.jl
+++ src/powerflow_types.jl
@@ -1,4 +1,7 @@
-struct ACPowerFlow end
+@kwdef struct ACPowerFlow
+    check_reactive_power_limits::Bool = false
+end
+
 struct DCPowerFlow end
 struct PTDFDCPowerFlow end
 struct vPTDFDCPowerFlow end
