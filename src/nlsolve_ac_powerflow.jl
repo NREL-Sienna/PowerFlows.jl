0a0aac2be6cff7257b072a422d3dfd2caccb5944
Jose Daniel Lara <jdlara@berkeley.edu>
2023-10-30 12:21:34 -0600

    update ac power flow methods

--- src/nlsolve_ac_powerflow.jl
+++ src/nlsolve_ac_powerflow.jl
@@ -4,12 +4,11 @@ Updates generators active and reactive power setpoints and branches active and r
 power flows (calculated in the From - To direction) (see
 [`flow_val`](@ref))
 
-Supports solving using Finite Differences Method (instead of using analytic Jacobian)
-by setting finite_diff = true.
 Supports passing NLsolve kwargs in the args. By default shows the solver trace.
 
 Arguments available for `nlsolve`:
 
+  - `get_connectivity::Bool`: Checks if the network is connected. Default true
   - `method` : See NLSolve.jl documentation for available solvers
   - `xtol`: norm difference in `x` between two successive iterates under which
     convergence is declared. Default: `0.0`.
@@ -31,12 +30,17 @@ solve_powerflow!(sys)
 solve_powerflow!(sys, method=:newton)
 ```
 """
-function solve_powerflow!(::ACPowerFlow, system::PSY.System; kwargs...)
+function solve_ac_powerflow!(system::PSY.System; kwargs...)
     #Save per-unit flag
     settings_unit_cache = deepcopy(system.units_settings.unit_system)
     #Work in System per unit
     PSY.set_units_base_system!(system, "SYSTEM_BASE")
-    res = _solve_powerflow(system, finite_diff; kwargs...)
+    data = PowerFlowData(
+        ACPowerFlow(),
+        system;
+        check_connectivity = get(kwargs, :check_connectivity, true),
+    )
+    res = _solve_powerflow(data; kwargs...)
     if res.f_converged
         write_powerflow_solution!(system, res.zero)
         @info("PowerFlow solve converged, the results have been stored in the system")
@@ -59,8 +63,6 @@ Returns the results in a dictionary of dataframes.
 res = solve_powerflow(sys)
 # Passing NLsolve arguments
 res = solve_powerflow(sys, method=:newton)
-# Using Finite Differences
-res = solve_powerflow(sys, finite_diff=true)
 ```
 """
 function solve_powerflow(
@@ -68,15 +70,16 @@ function solve_powerflow(
     system::PSY.System;
     kwargs...,
 )
-    ybus_kw = [:check_connectivity]
-    ybus_kwargs = (k for k in kwargs if first(k) ∈ ybus_kw)
-    nlsolve_kwargs = (k for k in kwargs if first(k) ∉ ybus_kw)
-
     #Save per-unit flag
     settings_unit_cache = deepcopy(system.units_settings.unit_system)
     #Work in System per unit
     PSY.set_units_base_system!(system, "SYSTEM_BASE")
-    res = _solve_ac_powerflow(system, finite_diff; kwargs...)
+    data = PowerFlowData(
+        ACPowerFlow(),
+        system;
+        check_connectivity = get(kwargs, :check_connectivity, true),
+    )
+    res = _solve_powerflow(data; kwargs...)
     if res.f_converged
         @info("PowerFlow solve converged, the results are exported in DataFrames")
         df_results = write_results(ACPowerFlow(), system, res.zero)
@@ -89,17 +92,12 @@ function solve_powerflow(
     return res.f_converged
 end
 
-function solve_powerflow()
-    pf!(res, x0)
-    if sum(res) > 10 * (N_BUS * 2)
-        _, ix = findmax(res)
-        bus_no = PSY.get_number(buses[(ix + 1) ÷ 2])
-        @warn "Initial guess provided results in a large initial residual. Largest residual at bus $bus_no"
-    end
+function _solve_powerflow(data::ACPowerFlowData; kwargs...)
+    nlsolve_kwargs = (k for k in kwargs if first(k) ∉ AC_PF_KW)
+    pf = PolarPowerFlow(data)
+    J = PowerFlows.PolarPowerFlowJacobian(data, pf.x0)
 
-    F0 = similar(x0)
-    df = NLsolve.OnceDifferentiable(pf!, jsp!, x0, F0, J0)
-    res = NLsolve.nlsolve(df, x0; kwargs...)
-
+    df = NLsolve.OnceDifferentiable(pf, J, pf.x0, pf.residual, J.Jv)
+    res = NLsolve.nlsolve(df, pf.x0; nlsolve_kwargs...)
     return res
 end
