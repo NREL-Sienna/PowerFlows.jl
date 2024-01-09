d9ee92ab02ece130b54b6d7c4a7c6196a60453fb
Jose Daniel Lara <jdlara@berkeley.edu>
2024-01-09 14:26:10 -0700

    time_step clean up

--- test/test_powerflow_data.jl
+++ test/test_powerflow_data.jl
@@ -8,9 +8,9 @@ end
 
 @testset "PowerFlowData multiperiod" begin
     sys = PSB.build_system(PSB.PSITestSystems, "c_sys14"; add_forecasts = false)
-    timesteps = 24
+    time_steps = 24
     # TODO: "multiperiod AC still to implement"
-    PowerFlowData(DCPowerFlow(), sys; timesteps = timesteps)
-    PowerFlowData(PTDFDCPowerFlow(), sys; timesteps = timesteps)
-    PowerFlowData(vPTDFDCPowerFlow(), sys; timesteps = timesteps)
+    PowerFlowData(DCPowerFlow(), sys; time_steps = time_steps)
+    PowerFlowData(PTDFDCPowerFlow(), sys; time_steps = time_steps)
+    PowerFlowData(vPTDFDCPowerFlow(), sys; time_steps = time_steps)
 end
