753de733f928eb036f422dd041ab2fba873712a4
Jose Daniel Lara <jdlara@berkeley.edu>
2024-01-09 14:25:44 -0700

    add abstract type

--- src/powerflow_types.jl
+++ src/powerflow_types.jl
@@ -1,7 +1,9 @@
-Base.@kwdef struct ACPowerFlow
+abstract type PowerFlowEvaluationModel end
+
+Base.@kwdef struct ACPowerFlow <: PowerFlowEvaluationModel
     check_reactive_power_limits::Bool = false
 end
 
-struct DCPowerFlow end
-struct PTDFDCPowerFlow end
-struct vPTDFDCPowerFlow end
+struct DCPowerFlow <: PowerFlowEvaluationModel end
+struct PTDFDCPowerFlow <: PowerFlowEvaluationModel  end
+struct vPTDFDCPowerFlow <: PowerFlowEvaluationModel  end
