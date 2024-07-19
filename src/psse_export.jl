551f9671fcb1a88a7f1f5b245031dc41a6f738d6
GabrielKS <23368820+GabrielKS@users.noreply.github.com>
2024-07-19 16:38:10 -0600

    Add `update_exporter!(..., System)`, extensively refactor tests

--- src/psse_export.jl
+++ src/psse_export.jl
@@ -1,13 +1,18 @@
-const PSSE_EXPORT_SUPPORTED_VERSIONS = [:v33, :v34]
+const PSSE_EXPORT_SUPPORTED_VERSIONS = [:v33]  # TODO add :v34
 
 """
-Structure to perform an export from a Sienna System plus optional updates from `PowerFlowData`
+Structure to perform an export from a Sienna System, plus optional updates from
+`PowerFlowData`, to the PSS/E format. Construct from a `System` and a PSS/E version, update
+using `update_exporter` with any new data as relevant, and perform the export with
+`write_export`.
 
 # Arguments:
-  - `base_system::PSY.System`: the system to be exported. Later updates may change power flow-related values but may not fundamentally alter the system
-  - `psse_version::Symbol`: the version of PSS/E to target, must be one of `PSSE_EXPORT_SUPPORTED_VERSIONS`
+  - `base_system::PSY.System`: the system to be exported. Later updates may change power
+    flow-related values but may not fundamentally alter the system
+  - `psse_version::Symbol`: the version of PSS/E to target, must be one of
+    `PSSE_EXPORT_SUPPORTED_VERSIONS`
 """
-struct PSSEExporter
+mutable struct PSSEExporter
     # Internal fields are very much subject to change as I iterate on the best way to do
     # this! For instance, the final version will almost certainly not store an entire System
     system::PSY.System
@@ -25,6 +30,42 @@ struct PSSEExporter
     end
 end
 
+function _validate_same_system(sys1::PSY.System, sys2::PSY.System)
+    return IS.get_uuid(PSY.get_internal(sys1)) == IS.get_uuid(PSY.get_internal(sys2))
+end
+
+"""
+Update the `PSSEExporter` with new `data`.
+
+# Arguments:
+  - `exporter::PSSEExporter`: the exporter to update
+  - `data::PSY.PowerFlowData`: the new data. Must correspond to the `System` with which the
+    exporter was constructor
+"""
+function update_exporter!(exporter::PSSEExporter, data::PowerFlowData)
+    # TODO
+    raise(IS.NotImplementedError("TODO"))
+end
+
+# TODO solidify the notion of sameness we care about here
+"""
+Update the `PSSEExporter` with new `data`.
+
+# Arguments:
+  - `exporter::PSSEExporter`: the exporter to update
+  - `data::PSY.System`: system containing the new data. Must be fundamentally the same
+  `System` as the one with which the exporter was constructed, just with different values
+"""
+function update_exporter!(exporter::PSSEExporter, data::PSY.System)
+    _validate_same_system(exporter.system, data) || throw(
+        ArgumentError(
+            "System passed to update_exporter must be the same system as the one with which the exporter was constructed, just with different values",
+        ),
+    )
+    exporter.system = deepcopy(data)
+end
+
+"Peform an export from the data contained in a `PSSEExporter` to the PSS/E file format."
 function write_export(
     exporter::PSSEExporter,
     scenario_name::AbstractString,
@@ -37,7 +78,7 @@ end
 
 # TODO remove duplication between here and Write_Sienna2PSSE
 "Calculate the paths of the (raw, metadata) files that would be written by a certain call to `write_export`"
-function get_paths(
+function get_psse_export_paths(
     scenario_name::AbstractString,
     year::Int,
     export_location::AbstractString,
