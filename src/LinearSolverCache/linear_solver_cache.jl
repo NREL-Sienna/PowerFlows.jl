15165687c95c4551f067a0c9ddfe747232206756
Luke Kiernan <Ljkier@umich.edu>
2025-02-26 14:42:52 -0700

    requested changes to linear solvers

--- src/LinearSolverCache/linear_solver_cache.jl
+++ src/LinearSolverCache/linear_solver_cache.jl
@@ -1,13 +1,12 @@
-const TIs = Union{Int32, Int64}
 """Abstract supertype for all cached linear solvers.
 Subtypes must implement: `symbolic_factor!`, `symbolic_refactor!`,
 `numeric_refactor!` (which doubles as `numeric_factor!`), and `solve!`."""
-abstract type LinearSolverCache{T <: TIs} end
+abstract type LinearSolverCache{T <: Signed} end
 
 function full_refactor!(
     cache::LinearSolverCache{T},
     A::SparseMatrixCSC{Float64, T},
-) where {T <: TIs}
+) where {T <: Signed}
     symbolic_refactor!(cache, A)
     numeric_refactor!(cache, A)
     return
@@ -16,7 +15,7 @@ end
 function full_factor!(
     cache::LinearSolverCache{T},
     A::SparseMatrixCSC{Float64, T},
-) where {T <: TIs}
+) where {T <: Signed}
     symbolic_factor!(cache, A)
     numeric_refactor!(cache, A)
     return
