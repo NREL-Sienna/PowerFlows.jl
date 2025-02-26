27505ad61f3ea6c3afc322156c88147af6fe4801
Luke Kiernan <Ljkier@umich.edu>
2025-02-25 20:32:08 -0700

    indexing in large initial residual warning

--- src/ac_power_flow.jl
+++ src/ac_power_flow.jl
@@ -81,7 +81,7 @@ function PolarPowerFlow(data::ACPowerFlowData, time_step::Int64)
 
     if sum(res) > 10 * (n_buses * 2)
         _, ix = findmax(res)
-        bus_no = data.bus_lookup[ix]
+        bus_no = data.bus_lookup[div(ix, 2, RoundUp)]
         @warn "Initial guess provided results in a large initial residual. Largest residual at bus $bus_no"
     end
 
