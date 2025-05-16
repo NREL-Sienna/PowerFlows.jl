01cbbf54ba2feb103c9c14f3c54ad442525bfe4e
Luke Kiernan <lkiernan@nrel.gov>
2025-05-16 15:12:08 -0600

    formatting

--- test/test_new_components.jl
+++ test/test_new_components.jl
@@ -6,13 +6,16 @@
         TModelHVDCLine => (PSISystems, "sys10_pjm_ac_dc"),
         TwoTerminalVSCLine => (PSSEParsingTestSystems, "pti_vsc_hvdc_test_sys"),
         SwitchedAdmittance => (PSSEParsingTestSystems, "pti_case24_sys"),
-        TwoTerminalLCCLine => (PSSEParsingTestSystems, "pti_two_terminal_hvdc_test_sys"),
-        DiscreteControlledACBranch => (PSSEParsingTestSystems, "pti_modified_case14_sys"),
+        TwoTerminalLCCLine =>
+            (PSSEParsingTestSystems, "pti_two_terminal_hvdc_test_sys"),
+        DiscreteControlledACBranch =>
+            (PSSEParsingTestSystems, "pti_modified_case14_sys"),
         TwoTerminalGenericHVDCLine => (PSITestSystems, "c_sys5_dc"),
         Transformer2W => (PSIDSystems, "psid_4bus_multigen"),
         Transformer3W => (PSSEParsingTestSystems, "pti_frankenstein_20_sys"),
         FACTSControlDevice => (PSSEParsingTestSystems, "pti_frankenstein_70_sys"),
-        PhaseShiftingTransformer => (PSSEParsingTestSystems, "pti_two_winding_mag_test_sys")
+        PhaseShiftingTransformer =>
+            (PSSEParsingTestSystems, "pti_two_winding_mag_test_sys"),
     )
     for (component, sysInfo) in componentToSys
         sys = build_system(sysInfo...)
