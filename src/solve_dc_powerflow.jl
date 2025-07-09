4c76b7272df67448512a10b22c774886238052b6
Roman Bolgaryn <roman.bolgaryn@nrel.gov>
2025-07-09 14:45:06 -0600

    small changes

--- src/solve_dc_powerflow.jl
+++ src/solve_dc_powerflow.jl
@@ -1,37 +1,3 @@
-const PTDFPowerFlowData = PowerFlowData{
-    PNM.PTDF{
-        Tuple{Vector{Int64}, Vector{String}},
-        Tuple{Dict{Int64, Int64}, Dict{String, Int64}},
-        Matrix{Float64},
-    },
-    PNM.ABA_Matrix{
-        Tuple{Vector{Int64}, Vector{Int64}},
-        Tuple{Dict{Int64, Int64}, Dict{Int64, Int64}},
-        PNM.KLU.KLUFactorization{Float64, Int64},
-    },
-}
-const vPTDFPowerFlowData = PowerFlowData{
-    PNM.VirtualPTDF{
-        Tuple{Vector{String}, Vector{Int64}},
-        Tuple{Dict{String, Int64}, Dict{Int64, Int64}},
-    },
-    PNM.ABA_Matrix{
-        Tuple{Vector{Int64}, Vector{Int64}},
-        Tuple{Dict{Int64, Int64}, Dict{Int64, Int64}},
-        PNM.KLU.KLUFactorization{Float64, Int64},
-    },
-}
-const ABAPowerFlowData = PowerFlowData{
-    PNM.ABA_Matrix{
-        Tuple{Vector{Int64}, Vector{Int64}},
-        Tuple{Dict{Int64, Int64}, Dict{Int64, Int64}},
-        PNM.KLU.KLUFactorization{Float64, Int64},
-    },
-    PNM.BA_Matrix{
-        Tuple{Vector{Int64}, Vector{String}},
-        Tuple{Dict{Int64, Int64}, Dict{String, Int64}}},
-}
-
 """
 Evaluates the power flows on each system's branch and updates the PowerFlowData structure.
 
