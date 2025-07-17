0a5ecc7ab37a39396b1d18a3cd4164395e0c4790
Luke Kiernan <lkiernan@nrel.gov>
2025-07-17 16:17:55 -0600

    eliminate extra allocations

--- src/RobustHomotopy/robust_homotopy_method.jl
+++ src/RobustHomotopy/robust_homotopy_method.jl
@@ -19,11 +19,13 @@ function _newton_powerflow(pf::ACPowerFlow{<:RobustHomotopyPowerFlow},
 
     Δt_k = get(kwargs, :Δt_k, DEFAULT_Δt_k)
     homHess = HomotopyHessian(data, time_step)
-    t_k_ref = homHess.t_k_ref
     x = homotopy_x0(data, time_step)
+    t_k = 0.0
 
-    t_k_ref[] += Δt_k
-    homHess(x, time_step)
+    # the sparse structure of the Hessian is different at t_k = 0.0 and t_k > 0.0
+    # so we need to increase t_k once before we initialize the MUMPS solver.
+    t_k += Δt_k
+    homHess(x, t_k, time_step)
 
     icntl = deepcopy(MUMPS.default_icntl)
     icntl[4] = 1 # report errors only
@@ -36,12 +38,12 @@ function _newton_powerflow(pf::ACPowerFlow{<:RobustHomotopyPowerFlow},
 
     success = true
     while true # go onto next t_k even if search doesn't terminate within max iterations.
-        converged_t_k, _ = _second_order_newton(homHess, time_step, x, mumps)
-        if t_k_ref[] == 1.0
+        converged_t_k, _ = _second_order_newton(homHess, t_k, time_step, x, mumps)
+        if t_k == 1.0
             success = converged_t_k
             break
         end
-        t_k_ref[] = min(t_k_ref[] + Δt_k, 1.0)
+        t_k = min(t_k + Δt_k, 1.0)
     end
     if !success
         @warn "RobustHomotopyPowerFlow failed to find a solution"
@@ -60,13 +62,13 @@ end
 
 sig3(x::Float64) = round(x; sigdigits = 3)
 
-function info_helper(homHess::HomotopyHessian, F_val::Float64, msg::String)
-    t_k = homHess.t_k_ref[]
+function info_helper(homHess::HomotopyHessian, t_k::Float64, F_val::Float64, msg::String)
     r_val = norm(homHess.pfResidual.Rv, Inf)
     @info "t_k = $(sig3(t_k)): $msg, F_k $(sig3(F_val)), residual $(sig3(r_val))"
 end
 
 function _second_order_newton(homHess::HomotopyHessian,
+    t_k::Float64,
     time_step::Int,
     x::Vector{Float64},
     mumps::Mumps;
@@ -75,42 +77,44 @@ function _second_order_newton(homHess::HomotopyHessian,
     tol::Float64 = get(kwargs, :tol, DEFAULT_NR_TOL)
 
     i, converged, stop = 0, false, false
-    F_val = F_value(homHess, x, time_step)
-    last_tk = homHess.t_k_ref[] == 1.0
+    F_val = F_value(homHess, t_k, x, time_step)
+    last_tk = t_k == 1.0
     δ = zeros(size(x, 1)) # PERF: allocating
     while i < maxIterations && !converged && !stop
         stop = _second_order_newton_step(
             homHess,
+            t_k,
             time_step,
             x,
             mumps,
             δ,
         )
-        F_val = F_value(homHess, x, time_step)
+        F_val = F_value(homHess, t_k, x, time_step)
         # TODO jump in tolerance. F_val ~ sum of squares, so...
         converged = (last_tk ? norm(homHess.pfResidual.Rv, Inf) : abs(F_val)) < tol
         i += 1
         if converged
-            info_helper(homHess, F_val, "converged")
+            info_helper(homHess, t_k, F_val, "converged")
         elseif i == maxIterations && !stop
-            info_helper(homHess, F_val, "max iterations")
+            info_helper(homHess, t_k, F_val, "max iterations")
         end
     end
     return converged, i
 end
 
 function _second_order_newton_step(homHess::HomotopyHessian,
+    t_k::Float64,
     time_step::Int,
     x::Vector{Float64},
     mumps::Mumps{Float64},
-    δ::Vector{Float64})
-    t_k = homHess.t_k_ref[]
-    F_val = F_value(homHess, x, time_step)
+    δ::Vector{Float64},
+)
+    F_val = F_value(homHess, t_k, x, time_step)
     last_step = t_k == 1.0
-    homHess(x, time_step)
+    homHess(x, t_k, time_step)
     if !last_step && dot(homHess.grad, homHess.grad) < GRAD_ZERO &&
        LinearAlgebra.isposdef(homHess.Hv) # stop case 1: hit local minimum.
-        info_helper(homHess, F_val, "local minimum")
+        info_helper(homHess, t_k, F_val, "local minimum")
         return true
     end
     _modify_hessian!(homHess.Hv, mumps)
@@ -124,10 +128,10 @@ function _second_order_newton_step(homHess::HomotopyHessian,
     # PERF: the line search is taking up 80%+ of the time in _second_order_newton_step
     # evidently I need a better (or better optimized) line search.
     # (The line search is non-allocating, except for the @warn message.)
-    α_star = line_search(x, time_step, homHess, δ)
+    α_star = line_search(x, time_step, homHess, t_k, δ)
     if !last_step && norm(δ * α_star) < INSUFFICIENT_CHANGE_IN_X
         # stop case 2: slow progress.
-        info_helper(homHess, F_val, "slow progress")
+        info_helper(homHess, t_k, F_val, "slow progress")
         return true
     end
     x .+= δ * α_star
