db6939ffc357405e32bf6e1fe4d214a2403a240b
mpolimeno <40783656+mpolimeno@users.noreply.github.com>
2025-07-25 12:13:12 -0600

    some refactoring of the LM implementation

--- src/levenberg-marquardt.jl
+++ src/levenberg-marquardt.jl
@@ -10,6 +10,12 @@ function _newton_powerflow(
     # setup: common code
     residual = ACPowerFlowResidual(data, time_step)
     x0 = improve_x0(pf, data, residual, time_step)
+    if OVERRIDE_x0 && :x0 in keys(kwargs) # for convergence testing
+       x0 .= get(kwargs, :x0, x0)
+       @warn "Overriding initial guess x0."
+       residual(x0, time_step)  # re-calculate residual for new x0: might have changed.
+    end
+ 
     J = ACPowerFlowJacobian(data, time_step)
     J(time_step)
     converged = norm(residual.Rv, Inf) < get(kwargs, :tol, DEFAULT_NR_TOL)
@@ -67,7 +73,7 @@ function _run_powerflow_method(
     linf = norm(residual.Rv, Inf)
     @debug "initially: sum of squares $(siground(resSize)), L ∞ norm $(siground(linf)), λ = $λ"
     while i < maxIterations && !converged && !isnan(λ)
-        λ = update!(x, residual, J, λ, time_step, maxTestλs)
+        λ = update_damping_factor!(x, residual, J, λ, time_step, maxTestλs)
         converged = !isnan(λ) && norm(residual.Rv, Inf) < tol
         i += 1
     end
@@ -76,13 +82,14 @@ function _run_powerflow_method(
     elseif i == maxIterations
         @error "The LevenbergMarquardtACPowerFlow solver didn't coverge in $maxIterations iterations."
     end
+
     return converged, i
 end
 
 # solving (J^T J + λ I) Δx = -J^T r can be numerically unstable
 # instead, take the least squares solution to [J; √λ I] * Δx  = [-r; zeros(n)]
 # see eg Kaltenbach's master's thesis on levenberg marquardt, p 13.
-function betterResidual(
+function compute_error(
     x::Vector{Float64},
     residual::ACPowerFlowResidual,
     J::ACPowerFlowJacobian,
@@ -92,8 +99,9 @@ function betterResidual(
 )
     residual(x, time_step)
     J(time_step)
+    
     A = vcat(J.Jv, sqrt(λ) * sparse(LinearAlgebra.I, size(J.Jv)))
-    b = vcat(-residual.Rv, zeros(size(J.Jv, 1)))
+    b = vcat(-residual.Rv, zeros(size(J.Jv, 2)))
     Δx = A \ b
 
     temp = residual.Rv .+ J.Jv * Δx
@@ -102,33 +110,19 @@ function betterResidual(
     residual(x_trial, time_step)
     newResidualSize = dot(residual.Rv, residual.Rv)
 
-    predicted_reduction = 0.5 * (residualSize - dot(temp, temp) - λ * dot(Δx, Δx))
-    actual_reduction = residualSize - newResidualSize
-    @assert predicted_reduction > 0
+    predicted_reduction = (residualSize - dot(temp, temp) - λ * dot(Δx, Δx))
+    actual_reduction = (residualSize - newResidualSize)
     ρ = actual_reduction / predicted_reduction
 
-    if actual_reduction > 0 && predicted_reduction > 0
-        step_size = norm(Δx)
-        linf = norm(residual.Rv, Inf)
-        @debug "sum of squares $(siground(newResidualSize)), L ∞ norm $(siground(linf)), λ = $(siground(λ)), ||Δx|| = $(siground(step_size))"
+    if ρ > 1e-4
         x .+= Δx
-        return ρ
-    else
-        residual(x, time_step)
     end
-    return -1.0
+
+    return ρ
 end
 
-"""Updates x following to Levenberg-Maquardt, returning the new value of λ.
-Current procedure for adjusting λ:
-(1) improvement with current λ => λ /= DAMPING_DECR and x += Δx.
-(2) else, λ *= DAMPING_INCR.
-    (2a) improvement with this λ => keep λ the same and x += Δx.
-    (2b) else, go back to (2).
-Here, \"improvement with λ\" means: `norm(F(x+Δx), 2) < norm(F(x), 2)`, where
-`Δx` is the solution to `(J' * J + λ I) Δx = -J'*F(x)`.
-"""
-function update!(
+
+function update_damping_factor!(
     x::Vector{Float64},
     residual::ACPowerFlowResidual,
     J::ACPowerFlowJacobian,
@@ -139,15 +133,47 @@ function update!(
     residual(x, time_step)
     residualSize = dot(residual.Rv, residual.Rv)
     J(time_step)
-    for _ in 1:maxTestλs
-        ρ = betterResidual(x, residual, J, λ, time_step, residualSize)
-        if ρ > 0
-            λ *= max(1 / 3, 1 - (2 * ρ - 1)^3)
+   
+    # This builds a diagonal matrix whose diagonal entries are from Jt*J
+    H_diag = LinearAlgebra.Diagonal(sum(abs2, J.Jv; dims=1)[:])
+
+    # Compute eigenvalues of the matrix to use their spacing to update the damping factor
+    eigvals = LinearAlgebra.diag(H_diag)
+    eigvals_sorted = sort(eigvals, by=x -> abs(x), rev=true)
+
+    # Log-scaled spacings
+    log_eigvals = log.(eigvals_sorted)
+    spacings = diff(log_eigvals)
+    spacing = maximum(spacings)       # Measure differences between consecutive singular values
+
+    # Use the largest spacing as a relaxation factor
+    relaxation_factor = maximum(spacing)
+    relaxation_factor = max(relaxation_factor, 4.0)  # Prevent very small factors
+
+    test_lambda::Int = 1
+    # https://www.sciencedirect.com/science/article/pii/S0142061518336342
+    λ = DEFAULT_λ_0*norm(residual.Rv) # this helps with some stability issues
+    # delayed gratification seems the more robust approach to the lambda update, and the more standard
+    while test_lambda<maxTestλs
+        ρ = compute_error(x, residual, J, λ, time_step, residualSize)
+        if ρ > 0.75 # good step
+            factor = (relaxation_factor>4.0) ? relaxation_factor : 4.0
+            λ /= factor
             λ = max(λ, 1.0e-8)
-            return λ
+            break
+        elseif ρ>=0.25 # okay step
+            break
+        else # bad step
+            factor = (relaxation_factor>0 && relaxation_factor<4.0) ? relaxation_factor : 4.0
+            λ *= factor
         end
-        λ *= 2.0
+
+       test_lambda += 1
     end
-    @error "Unable to improve: gave up after increasing damping factor $maxTestλs times."
-    return NaN
+    if test_lambda==maxTestλs
+        @error "Unable to improve: gave up after increasing damping factor $maxTestλs times."
+        λ = NaN
+    end
+
+    return λ
 end
