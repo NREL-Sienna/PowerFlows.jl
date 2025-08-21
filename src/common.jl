4d8f5b934b66a1338c34fc6b85ac75b175346438
mcllerena <marckcode@gmail.com>
2025-08-21 15:48:18 -0600

    fix: revert to using enumerate

--- src/common.jl
+++ src/common.jl
@@ -211,8 +211,7 @@ function make_dc_powerflowdata(
     calculate_loss_factors,
 )
     branch_type = Vector{DataType}(undef, length(branch_lookup))
-    for (ix, b) in zip(1:length(branch_lookup), PNM.get_ac_branches(sys))
-        println(ix, " ", b)
+    for (ix, b) in enumerate(PNM.get_ac_branches(sys))
         branch_type[ix] = typeof(b)
     end
     bus_reactivepower_bounds = Vector{Vector{Float64}}(undef, n_buses)
